4bdb4deaacf531d07bb157e79ce8fd007970ee54
Shawn Cicoria <github@cicoria.com>
2021-01-23 15:49:15 -0500

    logging and azurite

---
+++ runAzurite.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p ./.azurite/data
+
+docker run -it --rm -p 10000:10000 -v $(pwd)/.azurite/data:/data \
+    mcr.microsoft.com/azure-storage/azurite
+
+
+
+ #docker run -it --rm -p 10000:10000 -p 10001:10001 -p 10002:10002 -v $(pwd)/.azurite/data:/opt/azurite/folder cicorias/azureite:2
